fa44bfde0e3113cb9ade39b9c268a133d6a04c95
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-01-03 17:14:02 +0200

    lab 6 is done

--- lab6/index.js
+++ lab6/index.js
@@ -1,1 +1,25 @@
-console.log("init lab6")
+//1
+const inc = (x) => ++x;
+const twice = (x) => x * 2;
+const cube = (x) => x ** 3;
+
+process.on('uncaughtException', (err) => {
+  console.log('on uncaughtException: ' + err.message);
+  process.exit(1);
+});
+
+const pipe = (...fns) => {
+  for (let i = 0; i < fns.length; i++) {
+    if (typeof fns[i] !== 'function') throw new TypeError(`${fns[i]} is not a function`);
+  }
+  const calculate = (x) => {
+    let res = x;
+    fns.unshift(x);
+    res = fns.reduce((value, fn) => fn(value));
+    return res;
+  }
+  return calculate;
+}
+
+console.log(pipe(inc, twice, cube)(5));
+console.log(pipe(inc, 9, cube));
